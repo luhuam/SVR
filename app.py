771a6a9be0c160f28efb80fb30fcacc9f3fb6758
Lucero Marysol <67833726+luhuam@users.noreply.github.com>
2022-12-08 23:48:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+import streamlit as st
+from multiapp import MultiApp
+from apps import home,modelo1 # import your app modules here
 
+app = MultiApp()
+
+st.markdown("""
+# Inteligencia de Negocios - Equipo B
+""")
+# Add all your application here
+app.add_app("Home", home.app)
+app.add_app("Modelo SVR", modelo1.app)
+
+# The main app
+app.run()
