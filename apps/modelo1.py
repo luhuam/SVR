a28ba9395603408d33261457ca89726163a63a33
Lucero Marysol <67833726+luhuam@users.noreply.github.com>
2022-12-09 15:05:04 -0500

    Update modelo1.py

--- apps/modelo1.py
+++ apps/modelo1.py
@@ -26,41 +26,16 @@ def app():
     # Describiendo los datos
     st.subheader('Tabla de datos') 
     st.write(df)
-    st.subheader('Datos del 2004 al 2022') 
+    st.subheader('Resumen de datos') 
     st.write(df.describe())
-    st.subheader('Support Veector Regression') 
+    st.subheader('Support Vector Regression') 
+    st.write('Gráfica 1')
     def get_data(df):  
         df['Open']=df['Open'].astype(str)
         df['Open'] = df['Open'].str.split('-').str[2]
         df['Open'] = pd.to_numeric(df['Open'])
         return [ df['Open'].tolist(), df['Close'].tolist() ] 
     dates, prices = get_data(df)
-    st.write(get_data(df))
-    st.subheader('PROBANDOOO') 
-    def predict_prices(dates, prices, x):
-        dates = np.reshape(dates,(len(dates), 1)) # convert to 1xn dimension
-        x = np.reshape(x,(len(x), 1))
-    
-        svr_lin  = SVR(kernel='linear', C=1e3)
-        svr_poly = SVR(kernel='poly', C=1e3, degree=2)
-        svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
-    
-        # Fit regression model
-        svr_lin .fit(dates, prices)
-        svr_poly.fit(dates, prices)
-        svr_rbf.fit(dates, prices)
-    
-        plt.scatter(dates, prices, c='k', label='Data')
-        plt.plot(dates, svr_lin.predict(dates), c='g', label='Linear model')
-        plt.plot(dates, svr_rbf.predict(dates), c='r', label='RBF model')    
-        plt.plot(dates, svr_poly.predict(dates), c='b', label='Polynomial model')
-        plt.xlabel('Date')
-        plt.ylabel('Price')
-        plt.title('Support Vector Regression')
-        plt.legend()
-        plt.show()
-        return svr_rbf.predict(x)[0], svr_lin.predict(x)[0], svr_poly.predict(x)[0] 
-    st.subheader('Prediccion') 
-    predicted_price = predict_prices(dates, prices, [21])
-    st.pyplot(predicted_price)
+    st.write(dates,prices)
+    st.write('Gráfica 2')
     
